90ea61475e94e7cafd7706607b863ec479658d42
dipu Kumar Biswas <dipukumer143@gmail.com>
2024-11-15 19:37:14 +0600

    server

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId, CURSOR_FLAGS } = require('mongodb');
 const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors')
@@ -31,6 +31,47 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const Servicedatabase = client.db("car-doctor").collection("ServicsColaction");
+    const bookingCollaction = client.db("car-doctor").collection('servicBoking')
+   
+
+app.get('/services', async(req, res)=>{
+  const cursor = Servicedatabase.find()
+  const ruselt = await cursor.toArray()
+  res.send(ruselt)
+})
+
+app.get('/services/:id', async(req, res)=>{
+  const id = req.params.id;
+  const quary = {_id: new ObjectId(id)}
+//option file
+
+const options = {
+
+  // Include only the `title` and `imdb` fields in the returned document
+  projection: { title: 1, price: 1, description:1, img:1, service_id:1 },
+};
+
+
+  const result = await Servicedatabase.findOne(quary, options)
+  res.send(result)
+})
+
+
+//booking service stert
+
+app.post('/bookings', async (req, res)=>{
+   const booking = req.body;
+   console.log(booking)
+   const result = await bookingCollaction.insertOne(booking)
+   res.send(result)
+   
+})
+
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
